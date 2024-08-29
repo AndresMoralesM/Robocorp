a6a32a53c6b9b9284c278a24a4cd24919781cd05
Andres <andres.morales.1997.am@gmail.com>
2024-08-28 21:53:02 -0600

    Final submit

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 from robocorp.tasks import task
+from RPA.Robocloud.Items import Items
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -11,128 +12,177 @@ import pandas as pd
 import os
 import requests
 import re
+import time
+
+
+
+def click(driver,selector,attempts=2,waitTime=10):
+    
+    for i in range(0,attempts):
+        try:
+            button = WebDriverWait(driver, waitTime).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, selector))
+            )
+            driver.execute_script("arguments[0].scrollIntoView();", button)
+            button.click()
+            return
+        except Exception as e:
+            if i == attempts-1:
+                raise Exception(f"selector for buttom: {selector} could not be found after {attempts} attempts ")
+            continue
+
+def selectItem(driver,selector,option,attempts=2,waitTime=10):
+    for i in range(0,attempts):
+        try:
+            select_element = WebDriverWait(driver, waitTime).until(
+                    EC.presence_of_element_located((By.ID, selector))
+                )
+            select = Select(select_element)
+
+            # Seleccionar la opción por su valor
+            select.select_by_value(option)
+            return
+        except Exception as e:
+            if i == attempts-1:
+                raise Exception(f"selector for dropdown: {selector} could not be found after {attempts} attempts ")
+            continue
+
+def sendText(driver,selector,text,attempts=2,waitTime=10):
+    for i in range(0,attempts):
+        try:
+            input_field = WebDriverWait(driver, waitTime).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, selector))
+                )
+            input_field.send_keys(text) 
+            return
+        except Exception as e:
+            if i == attempts-1:
+                raise Exception(f"selector for Input Text: {selector} could not be found after {attempts} attempts ")
+            continue
 
 
 @task
-def minimal_task():
-    #path=requests.request("get", "https://cloud.robocorp.com/api/v1/workspaces/c9fdb73c-1e53-42f0-b2bd-f9f54eb0533b/assets/fbf28151-2972-4431-aa99-f5efb2868c46", headers={
-    #    "Content-Type": "application/json",
-    #    "Authorization": "RC-WSKEY kv38phq90FXtJ7QSDoMEDBUXyPD2qeDJIehXIIAiqQVLIWGGXOOHTyDF8XcQhrZKNUCPDBK1t1NdpvxFCTSZyiyvzVNpR4aJesN9EN6qhTJY0Zd3mPnUZdXAJd0NM"
-    #    })
-    #print(path.json())
-    edge_service = Service(executable_path="C:\\Users\\bella\\Downloads\\msedgedriver.exe")
-
-
-    edge_options = Options()
-    edge_options.add_argument('--ignore-certificate-errors')
-    edge_options.add_argument('--ignore-ssl-errors')
-	# Set up Edge WebDriver
-    driver = webdriver.Edge(service=edge_service)
-
-    driver.get("https://www.aljazeera.com/")
-
-    try:
-        button = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-testid="menu-trigger"]'))
-        )
-        button.click()
-
-        input_field = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'input[placeholder="Search"]'))
-        )
-
-        # Enviar un texto al campo de entrada
-        input_text = "dollars saving"
-        input_field.send_keys(input_text)
-        print("it was sent")
-
-        button = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'button[type="submit"].css-sp7gd'))
-        )
-        button.click()
-        
-        select_element = WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.ID, "search-sort-option"))
-        )
-
-        # Crear un objeto Select
-        select = Select(select_element)
-
-        # Seleccionar la opción por su valor
-        select.select_by_value("date")
-
-        
-
-        elements = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.search-result__list article'))
-        )
-
-        
-        if not os.path.exists('imagenes'):
-            os.makedirs('imagenes')
-
-        search_results = []
-        print(len(elements))
-        print("cuantos gg")
-        index=0
-        for element in elements:
-            
-            try:
-                title = element.find_element(By.CSS_SELECTOR, "a.u-clickable-card__link").text
-                img_element = element.find_element(By.CSS_SELECTOR, 'img.article-card__image.gc__image')
-                description = element.find_element(By.CSS_SELECTOR, "p").text
-                search_text=title.lower() + description.lower()
-
-                phraseCounter=0
-                for phrase in input_text.split():
-                    phraseCounter=len(search_text.split(phrase.lower()))+phraseCounter
-
-                patron_dinero = r"\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?|(?:\d{1,3}(?:,\d{3})*(?:\.\d{2})?\s*(?:usd|dollars))"
-
-                if re.search(patron_dinero, search_text.lower()):
-                    containsMoney=True
-                else:
-                    containsMoney=False
-                
-                # Obtén la URL de la imagen
-                img_url = img_element.get_attribute('src')
-                
-                # Descarga la imagen
-                img_data = requests.get(img_url).content
-                img_name = f'{title.split(" ")[0]}_{index + 1}.jpg'
-                index+=1
-                # Guarda la imagen en el directorio
-                with open(os.path.join('imagenes', img_name), 'wb') as handler:
-                    handler.write(img_data)
-
-
-                
-                #url = element.find_element(By.CSS_SELECTOR, "a.u-clickable-card__link").get_attribute("href")
-                date = driver.find_element(By.CSS_SELECTOR, 'div.date-simple.css-1yjq2zp span[aria-hidden="true"]').text
-                
-                print(date)
-                search_results.append({
-                    "title": title,
-                    "date": date,
-                    "description": description,
-                    "Picture_Name": img_name,
-                    "Phrases_Counter": phraseCounter,
-                    "Contains_Money": containsMoney
-
-                })
-            except Exception as e:
-                print(f"Error al extraer datos de un resultado: {e}")
-
-        # Convertir los resultados en un DataFrame de Pandas
-        df = pd.DataFrame(search_results)
-
-        # Mostrar el DataFrame
-        print(df)
-      
-
-        
+def getNews():
+    items = Items()
+    data = items.get_input_work_item()
+    path = data.payload.get("edgePath")
+    input_text = data.payload.get("inputPhrase")
+    section=data.payload.get("section")
+    fileName=data.payload.get("fileName")
+    print("Data from work item has been extracted sucessfully.")
+   
+  
     
-    except Exception as e:
-        print("El botón no se pudo encontrar o hacer clic:", e)
+    
+    for i in range(0,3):
+        try:
+            edge_service = Service(executable_path=path)	
+            driver = webdriver.Edge(service=edge_service)
+            driver.get("https://www.aljazeera.com/")# Open website
+            click(driver,'button[data-testid="menu-trigger"]')#Click on menu to unhide search bar
+            
+            sendText(driver,'input[placeholder="Search"]',input_text) # send search phrase 
 
-    driver.implicitly_wait(10) 
+            click(driver,'button[type="submit"].css-sp7gd')# Click on submit to search results
+            
+            selectItem(driver,"search-sort-option",section) # select date from dropdown options
+
+            #Try to get all the results from the page.
+            for i in range(0,9):
+                try:
+                    click(driver,'[data-testid="show-more-button"]')
+                except Exception as e:
+                    if i==0:
+                        raise Exception("There is no any match")
+                    else:
+                        print("There are less than 99 results.")
+            
+            # a delay to wait for all results to load    
+            time.sleep(8)           
+
+            #Get all articles
+            elements = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.search-result__list article'))
+            )
+
+            print(f"Articles for phrase: {input_text} have been founded ")
+            #check for images folder
+            if not os.path.exists('images'):
+                os.makedirs('images')
+                print(f"Images folder has been created {os.path.join('images')}")
+
+            search_results = []
+            
+            index=0
+
+            #For to get iterate in each article and extrac data
+            for element in elements:
+                
+                try:
+                    #Extract data
+                    title = element.find_element(By.CSS_SELECTOR, "a.u-clickable-card__link").text
+                    img_element = element.find_element(By.CSS_SELECTOR, 'img.article-card__image.gc__image')
+                    description = element.find_element(By.CSS_SELECTOR, "p").text
+                    search_text=title.lower() + description.lower()
+                    date = element.find_element(By.CSS_SELECTOR, 'div.date-simple.css-1yjq2zp span[aria-hidden="true"]').text
+
+                    #create counter phrase 
+                    phraseCounter=0
+                    for phrase in input_text.split():
+                        phraseCounter=len(search_text.split(phrase.lower()))+phraseCounter
+                    
+                    #regex for Possible formats: $11.1 | $111,111.11 | 11 dollars | 11 USD
+                    patron_dinero = r"\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?|(?:\d{1,3}(?:,\d{3})*(?:\.\d{2})?\s*(?:usd|dollars))" 
+
+                    if re.search(patron_dinero, search_text.lower()):
+                        containsMoney=True
+                    else:
+                        containsMoney=False
+                    
+                    # Get image url
+                    img_url = img_element.get_attribute('src')
+                    # Download image
+                    img_data = requests.get(img_url).content
+                    img_name = f'{title.split(" ")[0]}_{index + 1}.jpg'
+                    index+=1
+                    # save image in directory\images
+                    with open(os.path.join('images', img_name), 'wb') as handler:
+                        handler.write(img_data)
+
+                    
+                    #create table for results found
+                    search_results.append({
+                        "title": title,
+                        "date": date,
+                        "description": description,
+                        "Picture_Name": img_name,
+                        "Phrases_Counter": phraseCounter,
+                        "Contains_Money": containsMoney
+                    })
+                except Exception as e:
+                    print(f"Error could not extract data from an article: {e}")
+                    continue
+                
+            print("Data from articles has been extracted")
+            # Convert result into a pandas dataframe 
+            df = pd.DataFrame(search_results)
+            
+            file_path = os.path.join('output', fileName)
+
+            # Write the DataFrame to an Excel file
+            df.to_excel(file_path, index=False, engine='openpyxl')
+            print(f"Excel file has been created succesfully {str(file_path)}")
+
+            print("Bot has run succesfully")
+            break
+
+    
+
+            
+        
+        except Exception as e:
+            if i == 2:
+                raise Exception(f"{e} \n 3 attempts were performed from scratch")
+            continue
+            
+    
